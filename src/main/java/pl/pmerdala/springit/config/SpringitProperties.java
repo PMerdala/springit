6feb279288f248d75647aea69be8ee5050c26cbf
PMerdala <32525481+PMerdala@users.noreply.github.com>
2021-04-12 12:29:22 +0200

    Add properties class

---
+++ src/main/java/pl/pmerdala/springit/config/SpringitProperties.java
@@ -0,0 +1,7 @@
+package pl.pmerdala.springit.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("springit")
+public class SpringitProperties {
+}
