82311ee3f3b7d449bd7c4531e32bc5f94d132896
PMerdala <32525481+PMerdala@users.noreply.github.com>
2021-04-28 15:34:00 +0200

    add auditing to Link

--- src/main/java/pl/pmerdala/springit/SpringitApplication.java
+++ src/main/java/pl/pmerdala/springit/SpringitApplication.java
@@ -3,10 +3,12 @@ package pl.pmerdala.springit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import pl.pmerdala.springit.config.SpringitProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SpringitProperties.class)
+@EnableJpaAuditing
 public class SpringitApplication {
 
     public static void main(String[] args) {
--- src/main/java/pl/pmerdala/springit/domain/Link.java
+++ src/main/java/pl/pmerdala/springit/domain/Link.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,4 +31,16 @@ public class Link {
     private String url;
     @OneToMany(mappedBy = "link")
     private List<Comment> comments = new ArrayList<>();
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastUpdateDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
 }
