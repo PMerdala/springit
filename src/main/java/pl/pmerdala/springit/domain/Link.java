c5db77276291f8456362b2a22eca49b9b13a455d
PMerdala <32525481+PMerdala@users.noreply.github.com>
2021-04-28 10:40:49 +0200

    add JPA Mapping

--- src/main/java/pl/pmerdala/springit/domain/Link.java
+++ src/main/java/pl/pmerdala/springit/domain/Link.java
@@ -8,6 +8,9 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -21,4 +24,6 @@ public class Link {
     private String title;
     @NonNull
     private String url;
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 }
