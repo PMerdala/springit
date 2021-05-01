df249ab4a8797eb73f5f4c1f5e7952a3d25c470a
Paweł Merdała <p.merdala@interia.pl>
2021-05-02 00:02:34 +0200

    home use thymeleaf

--- src/main/java/pl/pmerdala/springit/controller/HomeController.java
+++ src/main/java/pl/pmerdala/springit/controller/HomeController.java
@@ -1,8 +1,16 @@
 package pl.pmerdala.springit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/home")
+    String home(Model model) {
+        model.addAttribute("title", "Hello, Thymeleaf!");
+        return "home";
+    }
 }
 
