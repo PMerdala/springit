b9c938be96fb755c3446d5fcb1bf465df0370647
Paweł Merdała <p.merdala@interia.pl>
2021-05-01 17:05:54 +0200

    static Home page

--- src/main/java/pl/pmerdala/springit/controller/CreateOrUpdateLinkData.java
+++ src/main/java/pl/pmerdala/springit/controller/CreateOrUpdateLinkData.java
@@ -3,12 +3,10 @@ package pl.pmerdala.springit.controller;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class CreateOrUpdateLinkData {
     private String title;
     private String url;
