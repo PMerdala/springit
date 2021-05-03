f4e33a31434f915189b01e308c8a18a1a7d3522c
Paweł Merdała <p.merdala@interia.pl>
2021-05-03 16:25:29 +0200

    Add security configure to use UserRepository

--- src/main/java/pl/pmerdala/springit/repositories/UserRepository.java
+++ src/main/java/pl/pmerdala/springit/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package pl.pmerdala.springit.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.pmerdala.springit.domain.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/pl/pmerdala/springit/security/SecurityConfiguration.java
+++ src/main/java/pl/pmerdala/springit/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package pl.pmerdala.springit.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -15,5 +23,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/link/submit/**").hasRole("ADMIN")
                 .and()
                 .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
 }
